c91bcc8358a100c549d1fc66e482a13cbc8314b7
huangkz <741386917@qq.com>
2017-12-06 18:09:24 +0800

    寻找第k小的数

--- src/main/java/org/hkz/alogrithm/other/Ksort.java
+++ src/main/java/org/hkz/alogrithm/other/Ksort.java
@@ -7,8 +7,36 @@ package org.hkz.alogrithm.other;
  * */
 public class Ksort {
 
-	public static void main(){
-		
+	public static void main(String[] args){
+		int[] a = {2,3,9,8,7,6,1,4,5,10};
+		int k = 5;
+		int num = getKminNum(a,k);
+		System.out.println(num);
+	}
+	public static int  getKminNum(int[] a, int k){	
+		return getkNum(a, 0, a.length - 1, k-1);
+	}
+	
+	public static int getkNum(int[] a, int i, int j, int k) {
+		int index = a[(i + j)/2];
+		int start = i , end = j,temp = 0;
+		while(start < end){
+			while (index < a[end]){
+				end--;
+			}
+			while (index > a[start]){
+				start++;
+			}
+			if (start < end ){
+				temp = a[start];
+				a[start] = a[end];
+				a[end] = temp;
+			}
+		}
+		a[start] = index;
+		if (start == k) return  a[k];
+		else if (k < start ) return getkNum(a, i,start,k); 
+		else return getkNum(a, start ,j, k );
 		
 	}
 	
