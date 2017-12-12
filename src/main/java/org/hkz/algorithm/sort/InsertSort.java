4c8c9952dc8862ac7171c94036ab02f50a7e55f5
huangkz <741386917@qq.com>
2017-12-12 17:59:53 +0800

    插入排序

---
+++ src/main/java/org/hkz/algorithm/sort/InsertSort.java
@@ -0,0 +1,32 @@
+package org.hkz.algorithm.sort;
+
+public class InsertSort {
+	public static void main(String[] args) {
+		int a[] = SortUtils.getArrays(10);
+		//int a[] = { 9 ,18 ,7, 10, 1 ,10, 8, 9, 7, 18 };
+		System.out.print("排序前的数据 : ");
+		SortUtils.print(a);
+		System.out.print("排序后的数据 : ");
+		a = insertSort1(a);
+		SortUtils.print(a);
+	}
+
+	public static int[] insertSort1(int[] a) {
+		int sentry = a[0] ,j=0, k=0;
+
+		for (int i = 1; i < a.length; i++) {
+			k =i; j = i;
+			sentry = a[j];
+			while ( j - 1 >= 0 && a[j - 1] > sentry ) {
+				j--;
+			}
+			while(k-1 >= 0 &&j < k){
+				a[k] = a[--k];
+			}
+			a[j] = sentry;
+			
+		}
+		return a;
+	}
+
+}
