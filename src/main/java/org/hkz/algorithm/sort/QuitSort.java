cc6555a53b3cf7fc62eca8041d3b01bf73e35688
huangkz <741386917@qq.com>
2017-12-13 18:17:08 +0800

    快速排序，待修改

---
+++ src/main/java/org/hkz/algorithm/sort/QuitSort.java
@@ -0,0 +1,49 @@
+package org.hkz.algorithm.sort;
+
+public class QuitSort {
+	public static void main(String[] args) {
+		//int a[] = SortUtils.getArrays(10);
+		int a[] = { 16, 5, 3, 4, 2, 1 ,1, 2, 15, 1 };
+		System.out.print("排序前的数据 : ");
+		SortUtils.print(a);
+		System.out.print("排序后的数据 : ");
+		quitSort1(a);
+		SortUtils.print(a);
+	}
+
+	public static void  quitSort1(int[] a) {
+		quicksort(a , 0 ,a.length - 1);
+		
+	}
+
+	public static int sort(int[] a, int start, int end) {
+		int index = a[start];
+		while (start < end){
+			while(start < end && a[end] >= index ){
+				end--;
+			}
+			a[start] = a[end];
+			while (start < end && a[start] <= index){
+				start++;
+			}
+			a[end] = a[start];
+		}
+		a[start] = index; 
+
+		return start;
+	}
+	
+	public static  void quicksort(int[] a, int i, int j){
+		if (i >= j )
+			return;
+		int p = sort(a,i,j);
+		sort(a , i , p-1);
+		
+		sort(a , p+1 , j);
+		
+	}
+	
+	
+	
+	
+}
